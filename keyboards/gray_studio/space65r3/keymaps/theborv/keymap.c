1d5bda4c3e3572221c6573a40b5574e86cb8a2cb
theborv <eirikborve@gmail.com>
2025-11-03 22:58:16 +0100

    Update keymap.c

--- keyboards/gray_studio/space65r3/keymaps/theborv/keymap.c
+++ keyboards/gray_studio/space65r3/keymaps/theborv/keymap.c
@@ -10,6 +10,9 @@ enum custom_keycodes {
     KIPAS = QK_KB_0,
 	POFF,
 	RBOOT,
+	MONG5,
+	LGTV,
+	EXTMON,
 };
 
 enum layers {
@@ -35,22 +38,94 @@ uint8_t mod_config(uint8_t mod) {
 void install_tap_dance_entries(void) {
     vial_tap_dance_entry_t td0 = { KC_SCLN, 
 								   EU_AE, 		// hold: æ
-								   S(EU_AE), 	// hold: Æ
+								   S(EU_AE), 	// doubletap: Æ
 								   KC_TRNS,
                                    TAP_TAPPING_TERM };
     vial_tap_dance_entry_t td1 = { KC_QUOT, 
                                    EU_OSTR, 	// hold: ø
-								   S(EU_OSTR), 	// hold: Æ
+								   S(EU_OSTR), 	// doubletap: Ø
 								   KC_TRNS,
                                    TAP_TAPPING_TERM };
     vial_tap_dance_entry_t td2 = { KC_LBRC,
                                    EU_ARNG, 	// hold: å
-								   S(EU_ARNG), 	// hold: Å
+								   S(EU_ARNG), 	// doubletap: Å
+								   KC_TRNS,
+                                   TAP_TAPPING_TERM };
+    vial_tap_dance_entry_t td3 = { KC_1,
+								   KC_F1,		// hold: F1
+								   KC_TRNS,
+								   KC_TRNS,
+                                   TAP_TAPPING_TERM };
+    vial_tap_dance_entry_t td4 = { KC_2,
+								   KC_F2,		// hold: F2
+								   KC_TRNS,
+								   KC_TRNS,
+                                   TAP_TAPPING_TERM };
+    vial_tap_dance_entry_t td5 = { KC_3,
+								   KC_F3,		// hold: F3
+								   KC_TRNS,
+								   KC_TRNS,
+                                   TAP_TAPPING_TERM };
+    vial_tap_dance_entry_t td6 = { KC_4,
+								   KC_F4,		// hold: F4
+								   KC_TRNS,
+								   KC_TRNS,
+                                   TAP_TAPPING_TERM };
+    vial_tap_dance_entry_t td7 = { KC_5,
+								   KC_F5,		// hold: F5
+								   KC_TRNS,
+								   KC_TRNS,
+                                   TAP_TAPPING_TERM };
+    vial_tap_dance_entry_t td8 = { KC_6,
+								   KC_F6,		// hold: F6
+								   KC_TRNS,
+								   KC_TRNS,
+                                   TAP_TAPPING_TERM };
+    vial_tap_dance_entry_t td9 = { KC_7,
+								   KC_F7,		// hold: F7
+								   KC_TRNS,
+								   KC_TRNS,
+                                   TAP_TAPPING_TERM };
+    vial_tap_dance_entry_t td10 = { KC_8,
+								   KC_F8,		// hold: F8
+								   KC_TRNS,
+								   KC_TRNS,
+                                   TAP_TAPPING_TERM };
+    vial_tap_dance_entry_t td11 = { KC_9,
+								   KC_F9,		// hold: F9
+								   KC_TRNS,
+								   KC_TRNS,
+                                   TAP_TAPPING_TERM };
+    vial_tap_dance_entry_t td12 = { KC_0,
+								   KC_F10,		// hold: F10
+								   KC_TRNS,
+								   KC_TRNS,
+                                   TAP_TAPPING_TERM };
+    vial_tap_dance_entry_t td13 = { KC_MINS,
+								   KC_F11,		// hold: F11
+								   KC_TRNS,
+								   KC_TRNS,
+                                   TAP_TAPPING_TERM };
+    vial_tap_dance_entry_t td14 = { KC_EQL,
+								   KC_F12,		// hold: F12
+								   KC_TRNS,
 								   KC_TRNS,
                                    TAP_TAPPING_TERM };
     dynamic_keymap_set_tap_dance(0, &td0);
     dynamic_keymap_set_tap_dance(1, &td1);
 	dynamic_keymap_set_tap_dance(2, &td2);
+	dynamic_keymap_set_tap_dance(3, &td3);
+	dynamic_keymap_set_tap_dance(4, &td4);
+    dynamic_keymap_set_tap_dance(5, &td5);
+    dynamic_keymap_set_tap_dance(6, &td6);
+	dynamic_keymap_set_tap_dance(7, &td7);
+	dynamic_keymap_set_tap_dance(8, &td8);
+	dynamic_keymap_set_tap_dance(9, &td9);
+	dynamic_keymap_set_tap_dance(10, &td10);
+	dynamic_keymap_set_tap_dance(11, &td11);
+	dynamic_keymap_set_tap_dance(12, &td12);
+	dynamic_keymap_set_tap_dance(13, &td13);
+	dynamic_keymap_set_tap_dance(14, &td14);
 }
 
 // send string to run batch files
@@ -74,6 +149,24 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         } else {
         }
         break;
+	case MONG5: // SamsungG5 only
+        if (record->event.pressed) {
+            SEND_STRING(SS_TAP(X_LGUI) SS_DELAY(100) "SamsungG5" SS_DELAY(100) SS_TAP(X_ENT));
+        } else {
+        }
+        break;
+	case LGTV: // LGTV only
+        if (record->event.pressed) {
+            SEND_STRING(SS_TAP(X_LGUI) SS_DELAY(100) "LGTV" SS_DELAY(100) SS_TAP(X_ENT));
+        } else {
+        }
+        break;
+	case EXTMON: // Extend monitors
+        if (record->event.pressed) {
+            SEND_STRING(SS_TAP(X_LGUI) SS_DELAY(100) "G5+LG" SS_DELAY(100) SS_TAP(X_ENT));
+        } else {
+        }
+        break;
     }
     return true;
 };
@@ -94,7 +187,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_L0] = LAYOUT(
-		QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC, KC_HOME,
+		QK_GESC, TD(3),   TD(4),   TD(5),   TD(6),   TD(7),   TD(8),   TD(9),   TD(10),  TD(11),  TD(12),  TD(13),  TD(14),  KC_BSPC, KC_BSPC, KC_HOME,
 		KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    TD(2),   KC_RBRC, KC_BSLS, KC_PGUP,
 		KC_CAPS,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    TD(0),   TD(1),   KC_ENT,           KC_PGDN,
 		KC_LSFT, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_DEL,
@@ -104,7 +197,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  QK_BOOT, QK_BOOT, KC_END,
 		_______,          UG_TOGG, UG_NEXT, UG_HUEU, UG_SATU, UG_VALU, UG_SPDU, _______, _______, _______, KIPAS,   _______, QK_RBT,  EE_CLR,  KC_PSCR,
 		_______,          _______, _______, UG_HUED, UG_SATD, UG_VALD, UG_SPDD, _______, _______, _______, _______, _______, _______,          KC_SCRL,
-		_______, _______, _______, _______, _______, _______, NK_OFF,  NK_ON,   _______, _______, _______, _______, _______,          KC_VOLU, KC_PAUS,
+		_______, _______, _______, _______, _______, _______, NK_OFF,  NK_ON,   MONG5,   LGTV,    EXTMON,  _______, _______,          KC_VOLU, KC_PAUS,
 		POFF,    _______, RBOOT,                              _______,                            _______, _______,          _______, KC_VOLD, KC_INS
 	)
 };
